543da39ec4d4344cc3d7f9920658507998ec31a2
Barabanov Nikita <s02200328@gse.cs.msu.ru>
2023-02-26 14:57:37 +0300

    Added -m launch with url or local file words list

--- 03_MergeRequirements/bullcows.py
+++ 03_MergeRequirements/bullcows.py
@@ -1,5 +1,16 @@
 import random
+import argparse
+import re
+import urllib.request
 
+# https://stackoverflow.com/questions/7160737/how-to-validate-a-url-in-python-malformed-or-not
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls, cows = 0, 0
@@ -26,9 +37,36 @@ def inform(format_string: str, bulls: int, cows: int) -> None:
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
     secret, cnt = random.choice(words), 1
+    print(secret)
     while (guess := ask("Введите слово: ", words)) != secret:
         cnt += 1
         inform("Быки: {}, Коровы: {}", *bullscows(guess, secret))
 
     inform("Быки: {}, Коровы: {}", *bullscows(guess, secret))
     return cnt
+
+
+def main(words: str, length: int = 5) -> None:
+    if re.match(regex, words) is not None:
+        file_name, _ = urllib.request.urlretrieve(words)
+    else:
+        file_name = words
+
+    try:
+        with open(file_name) as file:
+            words_list = [word.strip() for word in file.readlines() if len(word.strip()) == length]
+    except OSError:
+        print("Wrong path")
+        return
+
+    print(gameplay(ask, inform, words_list))
+
+
+
+
+if __name__ == "__main__":
+    bullscows_parser = argparse.ArgumentParser(prog="bullscows")
+    bullscows_parser.add_argument("words", type=str)
+    bullscows_parser.add_argument("length", default=5, type=int, nargs="?")
+    bullscows_args = bullscows_parser.parse_args()
+    main(bullscows_args.words, bullscows_args.length)
