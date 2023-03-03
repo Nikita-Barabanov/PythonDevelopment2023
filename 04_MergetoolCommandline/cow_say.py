1d1e5366d89bf59d9e25f361d7794a75378afcbf
Barabanov Nikita <s02200328@gse.cs.msu.ru>
2023-03-03 16:11:24 +0300

    Added complete

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -12,6 +12,14 @@ MAKE_BUBBLE_DEFAULTS = {"-b": ("cowsay", str),
                         "-d": (40, int),
                         "-w": (True, bool)}
 
+COWSAY_COMPLETE = COWTHINK_COMPLETE = {"-e": ["oo", "XX", "$$", "@@", "**", "--", "00", ".."],
+                                       "-c": cowsay.list_cows(),
+                                       "-T": ["  ", "U ", " U", "u ", " u", "||"]}
+
+MAKE_BUBBLE_COMPLETE = {"-b": ["cowsay", "cowthink"], "-d": [], "-w": ["True", "False"]}
+
+COMPLETE = {"cowsay": COWSAY_COMPLETE, "cowthink": COWTHINK_COMPLETE, "make_bubble": MAKE_BUBBLE_COMPLETE}
+
 
 def parse(args):
     return shlex.split(args, comments=True)
@@ -25,6 +33,11 @@ def get_optional_args(args, default_values):
         i += 2
 
     return opt_args
+
+
+def complete(text, line, begidx, endidx):
+    key, command = shlex.split(line)[-1] if begidx == endidx else shlex.split(line)[-2], shlex.split(line)[0]
+    return [s for s in COMPLETE[command][key] if s.startswith(text)]
 
 
 class Cowsayer(cmd.Cmd):
@@ -58,6 +71,9 @@ class Cowsayer(cmd.Cmd):
                                  width=opt_args["-d"],
                                  wrap_text=opt_args["-w"]))
 
+    def complete_make_bubble(self, text, line, begidx, endidx):
+        return complete(text, line, begidx, endidx)
+
     def do_cowsay(self, args):
         '''
         Returns the resulting cowsay string
@@ -72,6 +88,9 @@ class Cowsayer(cmd.Cmd):
         opt_args = get_optional_args(opt_args, COWSAY_DEFAULTS)
         print(cowsay.cowsay(message, cow=opt_args["-c"], eyes=opt_args["-e"], tongue=opt_args["-T"]))
 
+    def complete_cowsay(self, text, line, begidx, endidx):
+        return complete(text, line, begidx, endidx)
+
     def do_cowthink(self, args):
         """
         Returns the resulting cowthink string
@@ -85,6 +104,9 @@ class Cowsayer(cmd.Cmd):
         message, *opt_args = parse(args)
         opt_args = get_optional_args(opt_args, COWTHINK_DEFAULTS)
         print(cowsay.cowthink(message, cow=opt_args["-c"], eyes=opt_args["-e"], tongue=opt_args["-T"]))
+
+    def complete_cowthink(self, text, line, begidx, endidx):
+        return complete(text, line, begidx, endidx)
 
     def do_exit(self, args):
         """Exits from cowsay command line"""
