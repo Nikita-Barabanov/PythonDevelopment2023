fdbd0536587a8e0b2585ecf60eeba4336f0f4393
Barabanov Nikita <s02200328@gse.cs.msu.ru>
2023-03-14 00:22:53 +0300

    Added say, yield and quit

--- 05_DiffPatchNet/cowchat.py
+++ 05_DiffPatchNet/cowchat.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
 import cowsay
+import shlex
 
 clients = {}
 cows_to_peers = {}
+peers_to_cows = {}
 
 async def chat(reader, writer):
     me = "{}:{}".format(*writer.get_extra_info('peername'))
@@ -16,7 +18,7 @@ async def chat(reader, writer):
         for q in done:
             if q is send:
                 send = asyncio.create_task(reader.readline())
-                match q.result().decode().split(maxsplit=2):
+                match shlex.split(q.result().decode()):
                     case ["who", ]:
                         await clients[me].put(f"{list(cows_to_peers.keys())}")
                     case ["cows", ]:
@@ -27,17 +29,29 @@ async def chat(reader, writer):
                                 await clients[me].put("Cow name is already taken")
                             else:
                                 cows_to_peers[cow] = clients[me]
+                                peers_to_cows[clients[me]] = cow
                         else:
                             await clients[me].put("Unacceptable cow name")
-                    case ["say", cow, text]:
-                        pass
-                    case ["yield", text]:
-                        pass
+                    case ["say", cow, *text]:
+                        if cow in cows_to_peers:
+                            await cows_to_peers[cow].put(cowsay.cowsay("".join(text), cow=peers_to_cows[clients[me]]))
+                        else:
+                            await clients[me].put("There is no cow with such name")
+                    case ["yield", *text]:
+                        for cow in cows_to_peers:
+                            if cows_to_peers[cow] != clients[me]:
+                                await cows_to_peers[cow].put(cowsay.cowsay("".join(text), cow=peers_to_cows[clients[me]]))
                     case ["quit"]:
-                        pass
-                # for out in clients.values():
-                #     if out is not clients[me]:
-                #         await out.put(f"{me} {q.result().decode().strip()}")
+                        cow = peers_to_cows[clients[me]]
+                        cows_to_peers.pop(cow)
+                        peers_to_cows.pop(clients[me])
+                        send.cancel()
+                        receive.cancel()
+                        print(me, "DONE")
+                        del clients[me]
+                        writer.close()
+                        await writer.wait_closed()
+
             elif q is receive:
                 receive = asyncio.create_task(clients[me].get())
                 writer.write(f"{q.result()}\n".encode())
